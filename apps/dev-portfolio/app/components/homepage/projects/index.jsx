ec500fbc10eb756c780464f443a9606d7e53caf0
Billy Jacoby <billyjacoby@gmail.com>
2024-07-19 22:18:43 -0400

    wip

---
+++ apps/dev-portfolio/app/components/homepage/projects/index.jsx
@@ -0,0 +1,38 @@
+import ProjectCard from './project-card';
+
+import { projectsData } from '@/utils/data/projects-data';
+
+const Projects = () => {
+
+  return (
+    <div id='projects' className="relative z-50  my-12 lg:my-24">
+      <div className="sticky top-10">
+        <div className="absolute -top-3 left-0 size-[80px] translate-x-1/2 rounded-full bg-violet-100 opacity-30 blur-3xl"></div>
+        <div className="relative flex items-center justify-start">
+          <span className="absolute left-0 w-fit  rounded-md bg-[#1a1443] px-5 py-3 text-xl text-white">
+            PROJECTS
+          </span>
+          <span className="h-[2px] w-full bg-[#1a1443]"></span>
+        </div>
+      </div>
+
+      <div className="pt-24">
+        <div className="flex flex-col gap-6">
+          {projectsData.slice(0, 4).map((project, index) => (
+            <div
+              id={`sticky-card-${index + 1}`}
+              key={index}
+              className="sticky-card sticky mx-auto w-full max-w-2xl"
+            >
+              <div className="box-border flex items-center justify-center rounded shadow-[0_0_30px_0_rgba(0,0,0,0.3)] transition-all duration-[0.5s]">
+                <ProjectCard project={project} />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
---
+++ apps/dev-portfolio/next.config.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'media.dev.to',
+        pathname: '**',
+      },
+    ],
+  },
+};
